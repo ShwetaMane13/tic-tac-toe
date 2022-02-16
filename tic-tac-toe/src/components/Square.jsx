e8d3de473341c2f0b5bc27a92792a0830a7bc839
shweta mane <shwetamane1397@gmail.com>
2022-02-16 20:47:57 +0530

    tic tac toe done

---
+++ tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,8 @@
+export const Square = ({value, onClick}) => {
+
+    const style = value ? `squares ${value}` : `squares`;
+
+    return <button className={style} onClick={onClick}>
+        {value}
+    </button>
+}
